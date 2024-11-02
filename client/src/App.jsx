d5d79e86b54955a036a984afed80e3a45e009224
JavierLopezDev <javierlopezandujar2003@gmail.com>
2024-11-02 20:15:39 +0100

    User register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
-import React from "react"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import RegisterPage from "./pages/RegisterPage"
+import LoginPage from "./pages/LoginPage"
 
 function App() {
   return (
-    <div> App </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home page</h1>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/products" element={<h1>Products Page</h1>} />
+        <Route path="/add-product" element={<h1>New Product</h1>} />
+        <Route path="/product/:id" element={<h1>Update Product</h1>} />
+        <Route path="/profile" element={<h1>Profile</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
